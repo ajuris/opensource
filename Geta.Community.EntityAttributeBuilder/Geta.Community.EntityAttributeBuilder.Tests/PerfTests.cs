3a0fab327dfe206582e2ca244a398a1b393ca23d
Valdis Iljuconoks <valdis.iljuconoks@dotnet.lv>
2012-06-13 12:36:03 +0300

    Memory leak fix

---
+++ Geta.Community.EntityAttributeBuilder/Geta.Community.EntityAttributeBuilder.Tests/PerfTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Geta.Community.EntityAttributeBuilder.Tests
+{
+    [TestClass]
+    public class PerfTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int max = 1000;
+            int printOnCount = (max / 100);
+
+            Console.WriteLine("First {0}", GC.GetTotalMemory(false));
+
+            for (int i = 0; i < max; i++)
+            {
+                var metadata = new SampleType(-1).AsAttributeExtendable<SampleAttributeMetadata>();
+                if (i % printOnCount == 0)
+                {
+                    Console.WriteLine(GC.GetTotalMemory(false));
+                }
+            }
+
+            Console.WriteLine("Last {0}", GC.GetTotalMemory(true));
+        }
+    }
+}
